f4f81b3b84acd9a13a57eee5d223350b7cee6318
Isaac Dozier <isaactdozier@gmail.com>
2018-02-03 21:37:59 +0000

    v1.0.0-beta: Add Color choices. Add Tools. Move 'clear' button. Add dowload options. Fix drawing issues and stability.

--- src.js
+++ src.js
@@ -1,46 +1,82 @@
 //Build Toolbar
+var curTool = "pencil"
+
 var tools = [
     {
-        name:"paint-brush"
+        name:"clear",
+        icon:"window-close",
+        info: "Clear Canvas",
+        action: function(){
+                    ctx.clearRect(0,0,640,320);
+                }
     },
     {
-        name:"pencil-alt"
+        name:"paint-brush",
+        icon:"paint-brush",
+        info: "Thick Line",
+        action: function(){
+                    curTool = this.id
+                    ctx.lineWidth=5;
+                    highlightTool(this)
+                    resetTools(this)
+                }
+    },
+    {
+        name:"pencil",
+        icon:"pencil-alt",
+        info: "Thin Line",
+        action: function(){
+                    curTool = this.id
+                    
+                    ctx.lineWidth=1;
+                    highlightTool(this)
+                    resetTools(this)
+                    
+                }
     }
-    //{
-    //    name:"square"
-    //}
 ]
 
 document.getElementById('tool').innerHTML = btns(tools)
+//toolbar tool variables
+document.getElementById('clear').onclick       = tools[0].action
+document.getElementById('paint-brush').onclick = tools[1].action
+document.getElementById('pencil').onclick      = tools[2].action
 
 function btns(t){
 var tmp = ''
     for (var i = 0; i < t.length; i++) {
         if(tmp === ''){
-            tmp = '<button id="' + t[i].name + '"><i class="fa fa-' + t[i].name + '"></i></button>'
+            tmp = '<button style="background-color:#888888;border-color:#b3b3b3" title="' + t[i].info + '" id="' + t[i].name + '"><i class="fa fa-' + t[i].icon + '"></i></button>'
         } else {
-            tmp = tmp + '<button id="' + t[i].name + '"><i class="fa fa-' + t[i].name + '"></i></button>'
+            tmp = tmp 
+                + '<button style="background-color:#888888;border-color:#b3b3b3" title="' + t[i].info + '" id="' + t[i].name + '"><i class="fa fa-' + t[i].icon + '"></i></button>'
         }
     }
 return tmp
 }
 
-//toolbar tool variables
-////PAINT BRUSH - THICK LINE
-document.getElementById('paint-brush').onclick = function(){
-    resetTools()
-    ctx.lineWidth=5;
+highlightTool(document.getElementById(curTool))
+
+var userColor = 'black'                        //Default
+var color     = ['black', 'grey', 'white','blue', 'red', 'green', 'purple', 'pink', 'orange'] //Options
+
+document.getElementById('color').innerHTML = colors(color)
+
+function colors(c){
+var tmp = ''
+    for (var i = 0; i < c.length; i++) {
+        if(tmp === ''){
+           tmp = '<button onclick="resetColors(' + c[i] + ')" style="background-color:' + c[i] + '" title="' + c[i] + '" class="colors" id="' + c[i] + '"></button>'
+        } else {
+            tmp = tmp 
+                + '<button onclick="resetColors(' + c[i] + ')" style="background-color:' + c[i] + '" title="' + c[i] + '" class="colors" id="' + c[i] + '"></button>'
+        }
+    }
+return tmp
 }
-////PENCIL - THIN LINE
-document.getElementById('pencil-alt').onclick = function(){
-    resetTools()
-    ctx.lineWidth=1;
-}
-////CLEAR CANVAS BUTTON
-document.getElementById('clear').onclick = function(){
-    resetTools()
-    ctx.clearRect(0,0,640,320);
-}
+
+
+//End Buttons Toolbar
 
 //Build Canvas
 var click, squareStart = false
@@ -48,14 +84,11 @@ var offsetX, offsetY, cnt = 0
 var lastX = 0, lastY = 0
 var c = document.getElementById("draw")
 var ctx = c.getContext("2d")
-    ctx.strokeStyle = "#000000"
+    ctx.strokeStyle= "black"
     ctx.fillStyle="white";
     ctx.fillRect(0,0,640,320);
     
-////DRAW BOX
-//document.getElementById('square').onclick = function(){
-//    squareStart = true
-//}
+
 //set canvas dimensions
 document.getElementById('draw').width  = 640
 document.getElementById('draw').height = 320
@@ -77,18 +110,15 @@ function refresh(e) {
     //--Events--
     ////draw line
     if(click){
-        draw(event)
         
-    ctx.strokeRect(lastX,lastY,coorX(event),coorY(event))
+        draw(event)
     }
 }
 //Draw -- General
 function draw(e){
-    if(squareStart){
-        drawSquare(coorX(event),coorY(event))
-    } else {
+    
        ctx.lineTo(coorX(event),coorY(event)) 
-    }
+    
     ctx.stroke()
 }
 
@@ -112,6 +142,44 @@ function coorY(e){
 function replaceName(name){
     return name.split('-').join('')
 }
-function resetTools(){
-    squareStart = false
+function resetTools(t){
+    for (var i = 0; i < tools.length; i++) {
+       if(tools[i].name !== t.id){
+           document.getElementById(tools[i].name).style.backgroundColor = '#888888' 
+           document.getElementById(tools[i].name).style.borderColor = '#b3b3b3'
+           document.getElementById(tools[i].name).style.color = 'black'
+       } else {
+           
+       }
+    }
 }
+
+function highlightTool(t){
+    document.getElementById(t.id).style.color = userColor
+    document.getElementById(t.id).style.backgroundColor = "lightGrey"
+}
+
+function resetColors(c){
+    for (var i = 0; i < tools.length; i++) {
+        console.log(c.id)
+        if(tools[i].name === curTool){
+            document.getElementById(tools[i].name).style.color = c.id
+            resetTools(document.getElementById(curTool))
+            userColor = c.id
+            ctx.strokeStyle = c.id
+        }
+    }
+}
+
+function downloadCanvas(link, canvasId, filename) {
+    link.href = document.getElementById(canvasId).toDataURL();
+    link.download = filename;
+}
+
+document.getElementById('download').addEventListener('click', function() {
+    var fileName = document.getElementById('fileName').value
+    if (fileName == null || fileName == "") {
+        fileName = "drawing_app_" + (Math.floor(Math.random() * 1000000) + 1);
+    }
+    downloadCanvas(this, 'draw', fileName);
+}, false);
